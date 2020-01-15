a41e8d58a58a35185fc7cece1d4bfc29b6cfb2a6
rafa-guedes <ocerafa@gmail.com>
2020-01-15 10:36:50 -0300

    Defined css styling

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
 function App() {
 
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Github user</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
 
+          <div class="input-block">
+            <label htmlFor="techs">Technologies</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Save</button>
+
+        </form>
       </aside>
       <main>
-        
+
       </main>
     </div>
   );
