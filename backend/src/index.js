906b7d20dd18d1b18bddc4706a4965d5fb2cae36
rafa-guedes <ocerafa@gmail.com>
2020-01-15 11:44:10 -0300

    Allow backend to ureceive external requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes')
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-7lvp0.mongodb.net/w
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json()); // Make express understand json
 app.use(routes);
 
