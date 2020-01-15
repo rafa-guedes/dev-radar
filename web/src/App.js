67f891cfff84d86f0139ec5a9c7ca15f81dd4b0b
rafa-guedes <ocerafa@gmail.com>
2020-01-15 12:15:49 -0300

    Implemented api interaction interface

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -6,6 +7,8 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
   const [github_username, setGithubUsername] = useState('');
   const [techs, setTechs] = useState('');
   const [latitude, setLatitude] = useState('');
@@ -28,11 +31,37 @@ function App() {
     )
   }, [])
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data])
+  }
+
   return (
     <div id="app">
       <aside>
-        <strong>Cadastrar</strong>
-        <form>
+        <strong>Register</strong>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Github user</label>
             <input
@@ -87,53 +116,19 @@ function App() {
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/7799184?s=460&v=4" alt="Rafael Guedes"/>
-              <div className="user-info">
-                <strong>Rafael Guedes</strong>
-                <span>Python, Matlab</span>
-              </div>
-            </header>
-            <p>Oceanographer data scientist</p>
-            <a href="https://github.com/rafa-guedes">Access github profile</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/7799184?s=460&v=4" alt="Rafael Guedes"/>
-              <div className="user-info">
-                <strong>Rafael Guedes</strong>
-                <span>Python, Matlab</span>
-              </div>
-            </header>
-            <p>Oceanographer data scientist</p>
-            <a href="https://github.com/rafa-guedes">Access github profile</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/7799184?s=460&v=4" alt="Rafael Guedes"/>
-              <div className="user-info">
-                <strong>Rafael Guedes</strong>
-                <span>Python, Matlab</span>
-              </div>
-            </header>
-            <p>Oceanographer data scientist</p>
-            <a href="https://github.com/rafa-guedes">Access github profile</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/7799184?s=460&v=4" alt="Rafael Guedes"/>
-              <div className="user-info">
-                <strong>Rafael Guedes</strong>
-                <span>Python, Matlab</span>
-              </div>
-            </header>
-            <p>Oceanographer data scientist</p>
-            <a href="https://github.com/rafa-guedes">Access github profile</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name}/>
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Access github profile</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
