6af34c83c121a477264d855b23df4e6e827006b4
rafa-guedes <ocerafa@gmail.com>
2020-01-15 09:46:13 -0300

    Starting layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
-import Header from './Header'
-
-// Componente: funcao que retorna conteudo html
-// * One component per file (react rule)
-// * Isolated block of html, css and js that do not interfere with other components in the app
-
-// Estado: Information kept by the component (imutable)
-
-// Propriedade: Atribute in the component, such as title
-// * Information that parent component pass to children
+import React from 'react';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
 
   return (
-    <>
-      <h1>Counter: {counter} </h1>
-      <button onClick={incrementCounter}>Increment</button>
-    </>
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+        
+      </main>
+    </div>
   );
 }
 
